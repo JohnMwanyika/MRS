78c3c7b549c730e2f4a130e54261db748f94b428
John Mwanyika <mwanyikajohn@outlook.com>
2023-04-19 06:08:24 -0700

    create seeders for RequestStatuses

--- seeders/20230419124828-create-requestStatus.js
+++ seeders/20230419124828-create-requestStatus.js
@@ -3,6 +3,26 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('RequestStatuses',[
+      {
+        name: 'Completed',
+        description:'This request has been handled by the Admins',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Pending',
+        description:'This request is awayting approval of the Admins',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Declined',
+        description:'This request has been declined by the Admins, contact them for more information',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+    ])
     /**
      * Add seed commands here.
      *
@@ -15,6 +35,7 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('RequestStatuses', null, {});
     /**
      * Add commands to revert seed here.
      *
